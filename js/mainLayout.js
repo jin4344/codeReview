a1aadd5dea5fadeb53d0fe350006608dfbd3ecb1
jin4344 <makeing123@naver.com>
2023-01-12 17:48:18 +0900

    mainLayout 2 / 01.12

--- js/mainLayout.js
+++ js/mainLayout.js
@@ -1,11 +1,59 @@
-$(document).ready(function () {
-    $("#bodys").load("프로필.html");
-});
-//
-// 네비게이션 바의 목록 클릭 시 바디에 불러올 html 바꾸기
-// 아직 구상 중
-// $(() => {
-//     $("#bodys > #test").click(() => {
-//         $("#body").load("/");
+// $(document).ready(function () {
+//     // 로그인시 처음 띄워질 html로 바꿔 넣기
+//     $("#bodys").load("프로필.html");
+// });
+
+// $("#toProfile").click(function () {
+//     $.ajax({
+//         url: "/test.html",
+//         success: function (result) {
+//             alert("성공");
+//             $("#body").load("test.html");
+//         },
 //     });
 // });
+
+let goProgile = document.getElementById("toProfile");
+
+function gotoProfile() {
+    goProgile.addEventListener("click", function () {
+        $("#bodys").load("test.html");
+    });
+}
+
+//
+// 네비게이션 바의 목록 클릭 시 바디에 불러올 html 바꾸기
+// 아직 구상 중, ajax 말고는 없는 듯
+// $(() => {
+//     $("#toProfile").onClick(() => {
+//         $("#body").load("test.html");
+//     });
+// });
+
+//ajax으로 불러오기, 이건 서버랑 통신해야하는데
+// 이게 맞나?
+// $(document).ready(function () {
+//     $("#toProfile").click(function () {
+//         $.ajax({
+//             url: "/test.html",
+//             success: function (result) {
+//                 alert("성공");
+//                 $("#body").load("test.html");
+//             },
+//         });
+//     });
+// });
+$(document).ready(function () {
+    $("#menu2").click(function () {
+        $.ajax({
+            url: "menu.html",
+            success: function (result) {
+                var refine = $("#message2").html(result).find("li");
+                console.log(result);
+                console.log(refine);
+                console.log(typeof refine);
+                $("#message2").html(refine);
+            },
+        });
+    });
+});
