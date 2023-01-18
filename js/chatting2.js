639b49914c66a2253a9c52977801e2c6c373044f
jin4344 <makeing123@naver.com>
2023-01-18 19:27:24 +0900

    01.18

---
+++ js/chatting2.js
@@ -0,0 +1,4 @@
+$("#aaa").onclick = function () {
+    $(".chat_wrap").css("display", "yes");
+    $(".chatRoom").css("display", "none");
+};
--- js/mainLayout.js
+++ js/mainLayout.js
@@ -1,34 +1,20 @@
-// 로그인시 처음 띄워질 main html
-// 이러면 새로고침하면 main만 나오는데....
-// state 좀 공부 해야 할듯
 $(document).ready(function () {
-    document.getElementById("window").src = "프로필.html";
+    // 로그인시 처음 띄워질 main html
+    // 이러면 새로고침하면 main만 나오는데....
+    // state 좀 공부 해야 할듯
+    document.getElementById("window").src = "../지인씨/view/profile/img/profile1.html";
 });
 
-//ajax으로 불러오기, 이건 서버랑 통신해야하는데
-// 이게 맞나?
-// $(document).ready(function () {
-//     $("#toProfile").click(function () {
-//         $.ajax({
-//             url: "/test.html",
-//             success: function (result) {
-//                 alert("성공");
-//                 $("#body").load("test.html");
-//             },
-//         });
-//     });
-// });
-// $(document).ready(function () {
-//     $("#menu2").click(function () {
-//         $.ajax({
-//             url: "menu.html",
-//             success: function (result) {
-//                 var refine = $("#message2").html(result).find("li");
-//                 console.log(result);
-//                 console.log(refine);
-//                 console.log(typeof refine);
-//                 $("#message2").html(refine);
-//             },
-//         });
-//     });
-// });
+// 로그인 성공하면 "로그인하러 가기"를 간단 프로필로 만들기
+window.onload = function () {
+    // if(/*로그인 성공 */){
+    $("#profileBox").css({
+        width: "30%",
+        height: "90px",
+        margin: "8px",
+    });
+
+    $("#profileBox").append("<img src ='../지인씨/view/profile/img/0.png'/>");
+    $("#profileBox").append("<p>" + /*서버에서 사용자 이름 가져오기*/ +"</p>");
+    // }
+};
