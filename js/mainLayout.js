5be1a904ad62fb5f4ce20172f1390f13fbca37ec
jin4344 <makeing123@naver.com>
2023-01-19 17:18:27 +0900

    01.19

--- js/mainLayout.js
+++ js/mainLayout.js
@@ -8,14 +8,32 @@ $(document).ready(function () {
 // 로그인 성공하면 "로그인하러 가기"를 간단 프로필로 만들기
 window.onload = function () {
     // if(/*로그인 성공 */){
+    // 로그인 후 로그인하러가기 사이즈 변경
     $("#profileBox").css({
         width: "30%",
         height: "90px",
         margin: "8px",
     });
 
-    $("#profileBox").append("<img src ='../지인씨/view/profile/img/0.png'/>");
-    $("#profileBox").append("<p>" + /*서버에서 사용자 이름 가져오기*/ +"</p>");
-    // }
+    // 간단 프로필에 나올 프로필 사진과 사용자 닉네임 설정
+    /* 나중에 개인이 설정한 프로필사진 나오게 하기 */
+    $("#profileBox").append("<img src ='../지인씨/view/profile/img/0.png'/>"); //프로필 사진
+    $("#profileBox").append("<p>" + /*서버에서 사용자 이름 가져오기*/ "sf" + "</p>"); // 닉네임
+    $("#profileBox > p").css({
+        marginRight: "2vh",
+    });
+    $("#profileBox > a").remove();
+
+    // 간단 프로필에 나올 메일함 설정
+    const mailBox = document.createElement("div"); //메일함 wrap
+    const profilePoto = document.createElement("img"); // 메일함 사진
+    // const mailCheck = document.createElement("input"); // 메일 왔다는 신호 줄 input
+
+    // mailCheck.setAttribute("type", "checkbox");
+    profilePoto.setAttribute("src", "../img/imgForCss/mail.png");
+
+    $("#profileBox").append(mailBox);
+    $("#profileBox > div").append(profilePoto);
+    // $("#profileBox > div").append(mailCheck);
+    //  }
 };
-//
